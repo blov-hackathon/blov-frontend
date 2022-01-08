889874f84c889362a589d98052b30bda9f602557
adultlee <seoungin1228@gmail.com>
2022-01-08 15:48:51 +0900

    fix: modify styled css props

--- component/Button/index.jsx
+++ component/Button/index.jsx
@@ -2,10 +2,9 @@ import styled, { css } from "styled-components";
 
 const Button = styled.button`
   position: relative;
-  height: ${(props) => (props.height ? props.height : "50px")};
-  width: ${(props) => (props.width ? props.width : "30px")};
+  height: ${(props) => (props.height ? props.height : "50")}px;
+  width: ${(props) => (props.width ? props.width : "30")}px;
   border-radius: 10px;
-  font-size: 1rem;
   background-color: ${(props) =>
     props.backgroundColor ? props.backgroundColor : "white"};
   color: ${(props) => (props.color ? props.color : "black")};
