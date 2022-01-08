384f88030cbb68e2da0aefc9db26084ca706fddd
hyunjoebrother <hyunjoe31@gmail.com>
2022-01-08 19:00:22 +0900

    feat: add svg of login page

--- pages/login.js
+++ pages/login.js
@@ -2,29 +2,42 @@ import Layout from "../component/Layout";
 import Typography from "../component/Typography";
 import Button from "../component/Button";
 import Margin from "../component/Margin";
+import styled, { css } from "styled-components";
+
+const StyledMargin = styled(Margin)`
+  border: 1px solid red;
+  width: 60px;
+`;
+
+const StyledButton = styled(Button)`
+  border-radius: 100px;
+  border: none;
+`;
 
 export default function Login() {
   return (
     <Layout>
-      <Typography color="#aaa" size="16px">
+      <Margin size="30" />
+      <Typography color="#aaa" size="16">
         전화번호
       </Typography>
-      <Typography color="#aaa" size="16px">
+      <Typography color="#aaa" size="16">
         패스워드
       </Typography>
-      <Margin height="100px" />
-      <Typography color="#000" size="16px">
+      <Typography color="#000" size="16">
         로그인 상태 유지
       </Typography>
-      <Button backgroundColor="red" width="200px" height="32px">
-        <Typography color="#fff" size="24px">
+      <StyledMargin size="360" />
+      <StyledButton backgroundColor="red" width="360" height="50">
+        <Typography color="#fff" size="16">
           로그인
         </Typography>
-      </Button>
-      <Typography color="#505050" size="16px">
+      </StyledButton>
+      <Margin size="20" />
+      <Typography color="#505050" size="16">
         Blov가 처음이신가요?
       </Typography>
-      <Typography color="#DF2A19" size="16px">
+      <Typography color="#DF2A19" size="16">
         회원가입하기
       </Typography>
     </Layout>
