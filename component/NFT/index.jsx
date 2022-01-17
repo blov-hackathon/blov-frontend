f7e1d8a185c3abdccab9906ef584fc759c47a7ce
rxb8k <12183887@inha.edu>
2022-01-17 23:59:10 +0900

    fix: Connect mywallet page and NFT component
    헌혈 지갑 페이지에서 받아온 데이터(현재는 더미 데이터로 테스트 중, 추후 백 API 완성되면 연결해서 확인 필요)가 NFT 헌혈증 컴포넌트로 전달되어 각 데이터가 NFT 헌혈증에 알맞게 출력되는 것을 확인했습니다.

--- component/NFT/index.jsx
+++ component/NFT/index.jsx
@@ -5,71 +5,89 @@ import Typography from "../Typography";
 import Margin from "../Margin";
 
 const StyledTypography = styled(Typography)`
-  margin-left: 18px;
-  margin-right: 18px;
-  color: ${(props) => (props.color ? props.color : "#df2a19")};
+    margin-left: 18px;
+    margin-right: 18px;
+    color: ${(props) => (props.color ? props.color : "#df2a19")};
 `;
 
 const NFTstyle = styled.div`
-  height: 360px;
-  width: 240px;
-  border: solid 1px lightgray;
-  border-radius: 18px;
+    height: 360px;
+    width: 240px;
+    border: solid 1px lightgray;
+    border-radius: 18px;
 `;
 
 const NFTbackground = styled.div`
-  height: 250px;
-  width: 240px;
+    height: 250px;
+    width: 240px;
 `;
 
 const NFTfooter = styled.div`
-  height: 80px;
-  background-color: #df2a19;
-  vertical-align: bottom;
-  text-align: right;
-  border-bottom-left-radius: 18px;
-  border-bottom-right-radius: 18px;
+    height: 80px;
+    background-color: #df2a19;
+    vertical-align: bottom;
+    text-align: right;
+    border-bottom-left-radius: 18px;
+    border-bottom-right-radius: 18px;
+`;
+
+const NFTimagebox = styled.div`
+    height: 160px;
+    width: 240px;
+    // background-color: gray;
+`;
+
+const NFTimage = styled.img`
+    height: 150px;
+    width: 240px;
 `;
 
 const LogoImage = styled.img`
-  margin-left: 60px;
-  height: 40px;
+    //margin-left: 40px;
+    float: right;
+    margin-right: 10px;
+    height: 40px;
 `;
 
 const ShareLogo = styled.img`
-  margin-left: 5px;
-  height: 30px;
+    margin-top: 15px;
+    float: right;
+    margin-right: 20px;
+    margin-bottom: 10px;
+    height: 18px;
 `;
 
 export default function NFT(data) {
-  const [nftData, setNftData] = useState(data.data);
-  console.log(data);
-  console.log(nftData);
-  /*useEffect(() => {
-        axios
-            .get("URL이름 우헤헤")
-            .then(({ 데이터이름 }) => setNftData(데이터이름));
-    }, []);*/
-  return (
-    <NFTstyle>
-      <Margin size="15" />
-      <StyledTypography size="17">2021.01.19</StyledTypography>
-      <LogoImage src="/login/main-icon.svg" />
-      <NFTbackground>
-        <Margin size="160" />
-        <StyledTypography size="16">{nftData.name}}</StyledTypography>
-        <br />
-        <StyledTypography size="28">{nftData.blood}</StyledTypography>
-
-        <ShareLogo src="/login/main-icon.svg" />
-        <Margin size="20" />
-      </NFTbackground>
-      <NFTfooter>
-        <Margin size="40" />
-        <StyledTypography color="white">{nftData.number}</StyledTypography>
-      </NFTfooter>
-    </NFTstyle>
-  );
+    const [nftData, setNftData] = useState(data.data);
+    console.log(data);
+    return (
+        <NFTstyle>
+            <Margin size="15" />
+            <StyledTypography size="17">
+                {nftData.date ? nftData.date : ""}
+            </StyledTypography>
+            <LogoImage src="/login/main-icon.svg" />
+            <NFTbackground>
+                <NFTimagebox>
+                    <NFTimage src="#" />
+                </NFTimagebox>
+                <StyledTypography size="16">
+                    {nftData.bloodHouse ? nftData.bloodHouse : ""}
+                </StyledTypography>
+                <StyledTypography size="28">
+                    {nftData.bloodKind ? nftData.bloodKind : ""}
+                </StyledTypography>
+                <ShareLogo src="/mywallet/share-icon.svg" />
+                <Margin size="20" />
+            </NFTbackground>
+            <NFTfooter>
+                <Margin size="40" />
+                <StyledTypography color="white">
+                    {nftData.idNumber ? nftData.idNumber : ""}
+                </StyledTypography>
+            </NFTfooter>
+        </NFTstyle>
+    );
 }
 
 /*
@@ -78,8 +96,7 @@ get으로 받음
 2번
 로그인했는지받고
 데이터받고->useState
-*/
-/* 
+
 <Typography>{데이터.날짜}</Typography>
             <LogoImage src="/login/main-icon.svg" />
             <Typography>{데이터.혈액원}</Typography>
--- pages/mywallet.js
+++ pages/mywallet.js
@@ -1,74 +1,82 @@
 import Layout from "../component/Layout";
 import Typography from "../component/Typography";
 import NFT from "../component/NFT";
-import React, { Component, useEffect } from "react";
+import axios from "axios";
+import React, { Component, useEffect, useState } from "react";
 import styled, { css } from "styled-components";
 import _ from "lodash";
 import Margin from "../component/Margin";
 import Carousel from "nuka-carousel";
 
 const BoldTypography = styled(Typography)`
-  font-weight: bold;
+    font-weight: bold;
 `;
 
 const Header = styled.div`
-  width: 300px;
-  text-align: left;
+    width: 300px;
+    text-align: left;
 `;
 
 const NTFborder = styled.div`
-  flex-direction: column;
-  justify-content: center;
-  //box-shadow: -10px 20px 20px #000; // 그림자 이상해서 일단 주석 처리 ㅜㅜ
+    flex-direction: column;
+    justify-content: center;
+    //box-shadow: -10px 20px 20px #000; // 그림자 이상해서 일단 주석 처리
 `;
 
 const Menu = {
-  alignItems: "left",
+    alignItems: "left",
 };
 
 export default function Wallet() {
-  const array = [
-    { name: "인천 444", blood: "카리나의 피", number: "011111-23-44" },
-    { name: "인천 멋사 혈액원2222", blood: "전혈 1000ml", number: "01-23-442" },
-    { name: "인천 444", blood: "카리나의 피", number: "011111-23-44" },
-    { name: "인천 멋사 혈액원2222", blood: "전혈 1000ml", number: "01-23-442" },
-  ];
-  return (
-    <Layout>
-      <Header>
-        <Margin size="30" />
-        <BoldTypography size="24">나의 헌혈지갑</BoldTypography>
-        <Margin size="10" />
-        <div style={Menu}>
-          <Typography>NFT</Typography>
-          <Typography>시간순</Typography>
-        </div>
-      </Header>
-      <Margin size="40" />
-
-      <Carousel
-        height="390px"
-        width="250px"
-        slidesToShow={1} // 한 번에 보여줄 슬라이드의 개수
-        cellAlign="center"
-        renderCenterLeftControls={({ previousSlide }) => null}
-        renderCenterRightControls={({ nextSlide }) => null}
-        renderBottomCenterControls={() => null}
-      >
-        {_.map(array, (array) => (
-          <NTFborder>
-            <NFT data={array} />
-          </NTFborder>
-        ))}
-      </Carousel>
-    </Layout>
-  );
+    const [nftData, setNftData] = useState([]);
+    useEffect(() => {
+        axios.get("/mywallet").then(({ data }) => setNftData(data));
+    }, []);
+    const array = [
+        {
+            name: "이름",
+            date: "2022.01.17",
+            nftImage: "이미지",
+            bloodHouse: "혈액원명",
+            bloodKind: "전혈 300ML",
+            idNumber: "증서번호",
+        },
+        {
+            name: "이름2",
+            date: "2021.12.12",
+            nftImage: "이미지122",
+            bloodHouse: "혈액원명2",
+            bloodKind: "헌혈종류22",
+            idNumber: "증서번호1221",
+        },
+    ];
+    return (
+        <Layout>
+            <Header>
+                <Margin size="30" />
+                <BoldTypography size="24">나의 헌혈지갑</BoldTypography>
+                <Margin size="10" />
+                <div style={Menu}>
+                    <Typography>NFT</Typography>
+                    <Typography>시간순</Typography>
+                </div>
+            </Header>
+            <Margin size="40" />
+            <Carousel
+                height="390px"
+                width="240px"
+                slidesToShow={1} // 한 번에 보여줄 슬라이드의 개수
+                cellAlign="center"
+                renderCenterLeftControls={({ previousSlide }) => null}
+                renderCenterRightControls={({ nextSlide }) => null}
+                renderBottomCenterControls={() => null}
+            >
+                {_.map(array, (array) => (
+                    <NTFborder>
+                        <NFT data={array} />
+                    </NTFborder>
+                ))}
+            </Carousel>
+        </Layout>
+    );
 }
-
-/*
-useEffect로 axios로 받아서
-get으로 받음
-2번
-로그인했는지받고
-데이터받고->useState
-*/
