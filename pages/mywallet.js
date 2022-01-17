489809a898b967665bb6689cce95b9d60018dab0
Minhyeok <sonmh79@naver.com>
2022-01-17 20:49:37 +0900

    add: my-wallet qr-screen and animation

--- pages/mywallet.js
+++ pages/mywallet.js
@@ -1,11 +1,13 @@
 import Layout from "../component/Layout";
 import Typography from "../component/Typography";
 import NFT from "../component/NFT";
-import React, { Component, useEffect } from "react";
-import styled, { css } from "styled-components";
+import React, { Component, useEffect, useState } from "react";
+import styled, { css, keyframes } from "styled-components";
 import _ from "lodash";
 import Margin from "../component/Margin";
+import Button from "../component/Button";
 import Carousel from "nuka-carousel";
+import Link from "next/link";
 
 const BoldTypography = styled(Typography)`
   font-weight: bold;
@@ -26,6 +28,81 @@ const Menu = {
   alignItems: "left",
 };
 
+const qrPopUp = keyframes`
+  0% {
+    top: 100%
+  }
+  
+  100% {
+    top: 0%
+  }
+`;
+
+const BlackScreen = styled.div`
+  height: 100%;
+  width: 100%;
+  max-width: 375px;
+  background-color: rgba(0, 0, 0, 0.6);
+  z-index: 100;
+  position: absolute;
+  top: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 30px;
+  animation: ${(props) =>
+    props.active === true
+      ? css`
+          ${qrPopUp} 0.4s forwards
+        `
+      : ""};
+`;
+
+const QrScreen = styled.div`
+  height: 360px;
+  width: 100%;
+  border: solid 1px lightgray;
+  border-radius: 18px;
+  background-color: white;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const TitleFlex = styled.div`
+  display: flex;
+  justify-content: end;
+  width: 100%;
+`;
+
+const BackButton = styled.img`
+  width: 16px;
+  cursor: pointer;
+`;
+
+const QrImg = styled.img`
+  width: 70%;
+  border: 10px solid red;
+  border-radius: 24px;
+`;
+
+const DotLine = styled.div`
+  height: 30px;
+  border: 1px gray;
+  width: 100%;
+  border-style: none none dashed none;
+`;
+
+const QrTypography = styled(Typography)`
+  font-weight: bold;
+  word-break: break-all;
+`;
+
+const ClickButton = styled(Button)`
+  background-color: red;
+`;
+
 export default function Wallet() {
   const array = [
     { name: "인천 444", blood: "카리나의 피", number: "011111-23-44" },
@@ -33,6 +110,7 @@ export default function Wallet() {
     { name: "인천 444", blood: "카리나의 피", number: "011111-23-44" },
     { name: "인천 멋사 혈액원2222", blood: "전혈 1000ml", number: "01-23-442" },
   ];
+  const [active, setActive] = useState(false);
   return (
     <Layout>
       <Header>
@@ -61,6 +139,27 @@ export default function Wallet() {
           </NTFborder>
         ))}
       </Carousel>
+      <ClickButton onClick={() => setActive((active) => !active)}>
+        테스트 버튼
+      </ClickButton>
+      <BlackScreen active={active}>
+        <QrScreen>
+          <TitleFlex>
+            <Link href="#">
+              <BackButton
+                src="/temp/temp-close.svg"
+                onClick={() => setActive((active) => !active)}
+              />
+            </Link>
+          </TitleFlex>
+          <QrImg src="https://chart.googleapis.com/chart?chs=500x500&chld=M%7C1&cht=qr&chl={123}&choe=UTF-8" />
+          <DotLine></DotLine>
+          <Margin size="20" />
+          <QrTypography>
+            QEWTESGRGAREHGERSHSERAasdfadsasdfdsafasSFDSAT
+          </QrTypography>
+        </QrScreen>
+      </BlackScreen>
     </Layout>
   );
 }
