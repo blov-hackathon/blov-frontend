b1c2c928626a0e8ea6f40ab41ad6146fb349323a
adultlee <seoungin1228@gmail.com>
2022-01-22 22:30:05 +0900

    feat: add getDonorCard

--- pages/mywallet.js
+++ pages/mywallet.js
@@ -1,6 +1,6 @@
 import Layout from "../component/Layout";
 import Typography from "../component/Typography";
-import NFT from "../component/NFT";
+import NFT from "../component/NFT/";
 import axios from "axios";
 import React, { Component, useEffect, useState } from "react";
 import styled, { css, keyframes } from "styled-components";
@@ -10,65 +10,94 @@ import Button from "../component/Button";
 import Footer from "../component/Footer";
 import Carousel from "nuka-carousel";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const BoldTypography = styled(Typography)`
-    font-weight: bold;
+  font-weight: bold;
 `;
 
 const Header = styled.div`
-    width: 300px;
-    text-align: left;
+  width: 300px;
+  text-align: left;
 `;
 
 const NTFborder = styled.div`
-    flex-direction: column;
-    justify-content: center;
-    //box-shadow: -10px 20px 20px #000; // 그림자 이상해서 일단 주석 처리
+  flex-direction: column;
+  justify-content: center;
+  //box-shadow: -10px 20px 20px #000; // 그림자 이상해서 일단 주석 처리
 `;
 
 const Menu = {
-    alignItems: "left",
+  alignItems: "left",
 };
 
 export default function Wallet() {
-    const [nftData, setNftData] = useState([]);
-    useEffect(() => {
-        axios.get("/mywallet").then(({ Images }) => setNftData(Images));
-    }, []);
-    const testArray = [
-        { cardImage: "/mywallet/test-img.png", cardId: "0123" },
-        { cardImage: "null", cardId: "7896" },
-    ];
-    return (
-        <>
-            <Layout>
-                <Header>
-                    <Margin size="30" />
-                    <BoldTypography size="24">나의 헌혈지갑</BoldTypography>
-                    <Margin size="10" />
-                    <div style={Menu}>
-                        <Typography>NFT</Typography>
-                    </div>
-                </Header>
-                <Margin size="40" />
+  const [nftData, setNftData] = useState([]);
+  const [cardList, setCardList] = useState();
+  const router = useRouter();
 
-                <Carousel
-                    height="400px"
-                    width="240px"
-                    slidesToShow={1} // 한 번에 보여줄 슬라이드의 개수
-                    cellAlign="center"
-                    renderCenterLeftControls={({ previousSlide }) => null}
-                    renderCenterRightControls={({ nextSlide }) => null}
-                    renderBottomCenterControls={() => null}
-                >
-                    {_.map(testArray, (nftData) => (
-                        <NTFborder>
-                            <NFT data={nftData} />
-                        </NTFborder>
-                    ))}
-                </Carousel>
-            </Layout>
-            <Footer />
-        </>
-    );
+  const [token, setToken] = useState();
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const item = localStorage.getItem("token");
+      setToken(item);
+      if (!item) {
+        router.push("/login");
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    axios(`https://api-dev.blov.us/getDonorCard`, {
+      method: "GET",
+      crossDomain: true,
+      headers: {
+        Authorization: `Token ${token}`,
+      },
+    })
+      .then((res) => {
+        setCardList(res.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, [token]);
+
+  const testArray = [
+    { cardImage: "/mywallet/test-img.png", cardId: "0123" },
+    { cardImage: "null", cardId: "7896" },
+  ];
+  return (
+    <>
+      <Layout>
+        <Header>
+          <Margin size="30" />
+          <BoldTypography size="24">나의 헌혈지갑</BoldTypography>
+          <Margin size="10" />
+          <div style={Menu}>
+            <Typography>NFT</Typography>
+          </div>
+        </Header>
+        <Margin size="40" />
+
+        <Carousel
+          height="400px"
+          width="240px"
+          slidesToShow={1} // 한 번에 보여줄 슬라이드의 개수
+          cellAlign="center"
+          renderCenterLeftControls={({ previousSlide }) => null}
+          renderCenterRightControls={({ nextSlide }) => null}
+          renderBottomCenterControls={() => null}
+        >
+          {_.map(testArray, (nftData) => (
+            <NTFborder>
+              <NFT data={nftData} />
+            </NTFborder>
+          ))}
+        </Carousel>
+      </Layout>
+      <Footer />
+    </>
+  );
 }
